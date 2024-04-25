58eeb87cac9aabb7bda61d2c41673ea6732cd8a7
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2024-04-25 23:43:27 +0200

    tests

--- src/lib/timeWindowList.spec.ts
+++ src/lib/timeWindowList.spec.ts
@@ -1,17 +1,29 @@
 import { describe, expect, test } from '@jest/globals';
-import { classNames } from './classNames';
+import { TimeWindowList } from './timeWindowList';
 
-describe('classNames module', () => {
-  test('one class', () => {
-    expect(classNames('hello')).toBe('hello');
-  });
-  test('two classes', () => {
-    expect(classNames('hello', 'world')).toBe('hello world');
-  });
+const tenMinutes = 1000 * 60 * 10;
 
-  test('classes and falsy', () => {
-    expect(
-      classNames('hello', undefined, 'world', false, 'good', '', 'bye'),
-    ).toBe('hello world good bye');
+describe('TimeWindowList', () => {
+  test('keeping items within the window keeps them', () => {
+    const instance = new TimeWindowList<number>(10)
+      .add(0, 0)
+      .add(tenMinutes, 1);
+    expect(instance.length).toBe(2);
+  });
+  test('adding items moves the window and first items fall', () => {
+    const instance = new TimeWindowList<number>(10)
+      .add(0, 234324)
+      .add(tenMinutes, 233242)
+      .add(tenMinutes + 1000, 3453)
+      .add(tenMinutes + 2000, 345345)
+      .add(tenMinutes + 3000, 345345);
+    expect(instance.length).toBe(4);
+  });
+  test('the first items falls', () => {
+    const instance = new TimeWindowList<number>(10)
+      .add(0, 0)
+      .add(tenMinutes, 1)
+      .add(tenMinutes + 1000, 2);
+    expect(instance.at(0).value).toBe(1);
   });
 });
