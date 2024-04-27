3a420ae4c9bfde5f52a3459d5b7d171402de7cb4
Joan Llenas Masó <joan.llenas.maso@gmail.com>
2024-04-27 12:34:26 +0200

    asasa

--- src/data/loadAverageService.ts
+++ src/data/loadAverageService.ts
@@ -12,5 +12,6 @@ export function abortLoadAverage() {
 export async function getLoadAverage(): Promise<{ result: number }> {
   const response = await fetch(`${config.apiUrl}/load-average`, { signal });
   //return response.json();
+  //TODO: remove mock!!!
   return Promise.resolve({ result: Math.random() });
 }
